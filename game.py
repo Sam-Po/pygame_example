78c050d8366fc40c33e7d909ce5173e10747560e
pteacher <subfear@mail.ru>
2020-12-08 11:34:42 +0600

    add ball img and paddle moving

--- game.py
+++ game.py
@@ -12,10 +12,9 @@ y = 50
 r = 20
 dx = 1
 dy = 1
-rc = randint(0, 255)
-gc = randint(0, 255)
-bc = randint(0, 255)
-image = pygame.image.load(os.path.join(path, '2.jpg'))
+pad_x = 200
+pad_dx = 0
+image = pygame.image.load(os.path.join(path, 'tennis.png'))
 imh, imw = image.get_rect().size
 
 while run:
@@ -24,20 +23,20 @@ while run:
             run = False
         elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    dx = -1
+                    pad_dx = -0.04
                 if event.key == pygame.K_RIGHT:
-                    dx = 1
-                if event.key == pygame.K_UP:
-                    dy = -1
-                if event.key == pygame.K_DOWN:
-                    dy = 1
+                    pad_dx = 0.04
+    pad_x += pad_dx
+    if pad_x > 500 - 80 or pad_x < 0:
+        pad_dx = 0
 
-    x += 0.04 * dx
-    y += 0.04 * dy
-    
+    x += 0.03 * dx
+    y += 0.03 * dy
+
     screen.fill((255, 255, 255))
     screen.blit(image, (x, y))
-    # pygame.draw.circle(screen, (rc, gc, bc), (int(x), int(y)), r)
+    pygame.draw.rect(screen, (0, 0, 0), [pad_x, 450, 80, 10])
+
 
     if x > (500 - imh) or x < 0:
         rc = randint(0, 255)
@@ -48,5 +47,8 @@ while run:
         rc = randint(0, 255)
         gc = randint(0, 255)
         bc = randint(0, 255)
+        dy *= -1
+    if (x + imw / 2) > pad_x and (x + imw / 2) < (pad_x + 80)\
+        and (y + imh) > 450 and (y + imh) < 452:
         dy *= -1
     pygame.display.flip()
