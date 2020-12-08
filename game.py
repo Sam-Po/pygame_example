80ff45f7fa53a5241b973c51eeec6267de45a8c6
pteacher <subfear@mail.ru>
2020-12-08 11:48:12 +0600

    score text and todos added

--- game.py
+++ game.py
@@ -2,6 +2,12 @@ import pygame
 import os
 from random import randint
 
+
+def show_score(scene, score):
+    font = pygame.font.Font(None, 36)
+    text = font.render("Score: " + str(score), 1, (0, 0, 0))
+    scene.blit(text, (400, 20))
+
 path = os.path.dirname(os.path.realpath(__file__))
 
 pygame.init()
@@ -16,6 +22,7 @@ pad_x = 200
 pad_dx = 0
 image = pygame.image.load(os.path.join(path, 'tennis.png'))
 imh, imw = image.get_rect().size
+show_score(screen, 0)
 
 while run:
     for event in pygame.event.get():
@@ -23,15 +30,15 @@ while run:
             run = False
         elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    pad_dx = -0.04
+                    pad_dx = -0.2
                 if event.key == pygame.K_RIGHT:
-                    pad_dx = 0.04
+                    pad_dx = 0.2
     pad_x += pad_dx
     if pad_x > 500 - 80 or pad_x < 0:
         pad_dx = 0
 
-    x += 0.03 * dx
-    y += 0.03 * dy
+    x += 0.1 * dx
+    y += 0.1 * dy
 
     screen.fill((255, 255, 255))
     screen.blit(image, (x, y))
@@ -51,4 +58,10 @@ while run:
     if (x + imw / 2) > pad_x and (x + imw / 2) < (pad_x + 80)\
         and (y + imh) > 450 and (y + imh) < 452:
         dy *= -1
+
+    # TODO: ADD CONDITION FOR GAME OVER - HITTING BOTTOM
+    # TODO: ADDING POINTS
+    # TODO: BALL SHOULD CHANGE THE SPEED IF PLAYER SCORES EACH 10 PTS
+    # BONUS TODO: ADD BRICKS TO BREAK
+    show_score(screen, 0)
     pygame.display.flip()
